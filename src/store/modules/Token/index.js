06473f9d4300769cdefdab92aa315126c7311ba2
oppabgnamdz <oppabgnamdz@gmail.com>
2021-04-01 16:25:07 +0700

    add space line

--- src/store/modules/Token/index.js
+++ src/store/modules/Token/index.js
@@ -1,16 +1,19 @@
 const state = {
   token: "",
 };
+
 const mutations = {
   ADD_TOKEN(state, token) {
     state.token = token;
   },
 };
+
 const actions = {
   addToken({ commit }, token) {
     commit("ADD_TOKEN", token);
   },
 };
+
 const getters = {
   token: (state) => state.token,
 };
